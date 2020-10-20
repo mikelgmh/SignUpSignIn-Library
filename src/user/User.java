ab8f802fa66f02bd2eadce4a536ef7549cd0e7e3
Iker <theofficialdork@hotmail.com>
2020-10-20 09:45:11 +0200

    Creada las clases principales

--- src/user/User.java
+++ src/user/User.java
@@ -5,10 +5,92 @@
  */
 package user;
 
+import java.util.Date;
+
 /**
  *
- * @author Mikel
+ * @author Iker
  */
 public class User {
-    
+    private Integer id;
+    private String login;
+    private String email;
+    private String fullName;
+    private Boolean status;
+    private Integer privilege;
+    private String password;
+    private Date lastAccess;
+    private Date lastPasswordChange;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public void setPrivilege(Integer privilege) {
+        this.privilege = privilege;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setLastAccess(Date lastAccess) {
+        this.lastAccess = lastAccess;
+    }
+
+    public void setLastPasswordChange(Date lastPasswordChange) {
+        this.lastPasswordChange = lastPasswordChange;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public Integer getPrivilege() {
+        return privilege;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Date getLastAccess() {
+        return lastAccess;
+    }
+
+    public Date getLastPasswordChange() {
+        return lastPasswordChange;
+    }
 }
