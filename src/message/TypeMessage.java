2cbf97db81017b6ab0628b87b7841296ca6ca2cd
Imanol <56909426+moxarlos@users.noreply.github.com>
2020-10-24 15:44:25 +0200

    Refactor Enum

---
+++ src/message/TypeMessage.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Imanol
+ */
+public enum TypeMessage implements Serializable {
+    SIGN_IN,
+    SIGN_UP,
+    REGISTER_OK, 
+    USER_EXISTS,
+    MAIL_EXISTS,
+    LOGIN_OK,
+    LOGIN_ERROR,
+    CONNECTION_ERROR, 
+    MAX_CONNECTIONS_REACHED;
+}
